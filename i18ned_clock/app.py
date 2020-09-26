f97cb63a8780bbf1a44d7dca18f6ee6514024d0b
Mohammed Abdulkereem <mohammed-io@outlook.com>
2020-09-26 20:35:55 +0300

    Introduced new API

--- i18ned_clock/app.py
+++ i18ned_clock/app.py
@@ -5,7 +5,6 @@ from threading import Thread
 import asyncio
 
 
-
 class TimeZone(object):
     def __init__(self, timezone):
         self.timezone = timezone
@@ -28,9 +27,17 @@ class Clock(object):
         self.timezone = timezone
         self.timezoneObj = TimeZone(self.timezone)
 
+    def paint(self, frame):
+        localTime = time.localtime()
+
+        if frame >= 30:
+            return time.strftime("%H:%M:%S (GMT+{1})".format(self.timezone), localTime)
+        else:
+            return time.strftime("%H %M %S (GMT+{1})".format(self.timezone), localTime)
+
+
     def __str__(self, colons=True):
         return self.timezoneObj.calculate_current_time(colons)
-
 
 
 
@@ -42,18 +49,22 @@ async def init_screen(stdscr, clock, pos_y, pos_x):
             raise Exception(f"You can only set positions smaller than {y_limit} for y, and {x_limit} for x")
 
 
-    num = 0
+    frame = 0
+
     while True:
+        clock.paint()
         stdscr.addstr(pos_y, pos_x, clock.__str__(), curses.A_BOLD)
-        await asyncio.sleep(0.5)
+        # await asyncio.sleep(0.5)
+        # await asyncio.sleep(1/60)
+
+        frame = (frame + 1) % 60
+        # ..... clock.paint()
+
         stdscr.refresh()
         stdscr.addstr(pos_y,pos_x, clock.__str__(colons=False), curses.A_BOLD)
         await asyncio.sleep(0.5)
         stdscr.refresh()
 
-        num += 1
-        if num >= 10:
-            break
     
 
 clock1 = Clock(3)
