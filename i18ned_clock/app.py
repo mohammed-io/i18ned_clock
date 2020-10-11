d4540756e47ce3f5c4840c0e1300e5bc669ec174
sumayya55 <sumayya.kamal55@gmail.com>
2020-10-11 11:43:50 +0300

    rows and columns concept applied

--- i18ned_clock/app.py
+++ i18ned_clock/app.py
@@ -2,13 +2,12 @@ import curses
 import asyncio
 from datetime import datetime
 import pytz
-
+import os
   
 
 class Clock(object):
     def __init__(self, timezone):
-        self.timezone = timezone
-        self.tz = pytz.timezone(self.timezone)
+        self.tz = pytz.timezone(timezone)
 
     def paint(self, frame):
         if frame >= 30:
@@ -17,32 +16,62 @@ class Clock(object):
             return datetime.now(tz=self.tz).strftime("%H:%M:%S (UTC%Z)")
 
 
-
-async def init_screen(stdscr, clock, pos_y, pos_x):
+async def init_screen(stdscr, clock, y=0, x=0):
     curses.curs_set(0) 
-    
-    if pos_y >= 15 or pos_x >= 150:
-            y_limit = 15; x_limit = 150
-            raise Exception(f"You can only set positions smaller than {y_limit} for y, and {x_limit} for x")
 
     frame = 0
+    num = 0
     while True:
-        stdscr.addstr(pos_y, pos_x, clock.paint(frame), curses.A_BOLD)
-        await asyncio.sleep(1/60)
-        stdscr.refresh()
+        try:
+            stdscr.addstr(y, x, clock.paint(frame), curses.A_BOLD)
+            await asyncio.sleep(1/60)
+            stdscr.refresh()
+            frame = (frame + 1) % 60
+            num += 1
+            if num == 240:
+                break
+        except:
+            raise Exception('Window size is small.')
+            
 
-        frame = (frame + 1) % 60
-        
+class Tiles(object):
+    y = 0
+    x = 0
+    
+    def __init__(self, stdscr):
+        self.stdscr = stdscr
+   
+    async def row(self, clocklist):
+        resultedCoroutines = []
+        x = Tiles.x
+        for c in clocklist:
+            resultedCoroutines.append(init_screen(self.stdscr, c, y=Tiles.y, x=x))
+            x += 30
+        Tiles.y += 3
+        return resultedCoroutines
+
+    async def column(self, clocklist):
+        resultedCoroutines = []
+        y = Tiles.y
+        for c in clocklist:
+            resultedCoroutines.append(init_screen(self.stdscr, c, y=y, x=Tiles.x))
+            y += 3
+        Tiles.x += 30
+        return resultedCoroutines
 
 
 clock1 = Clock("Asia/Baghdad")
 clock2 = Clock("Asia/Kabul")
 
 async def main(stdscr):
-    await asyncio.gather(init_screen(stdscr, clock1, 0, 10), init_screen(stdscr, clock2, 0, 40))
-    
+    t = Tiles(stdscr) 
+
+    row1 = await t.row([clock1, clock2, clock1, clock2, clock1])
+    row2 = await t.row([clock1, clock1, clock1, clock1, clock1])
+    col1 = await t.column([clock1, clock2, clock1])
+    col2 = await t.column([clock1, clock2, clock1])
+    row3 = await t.row([clock1, clock1, clock1, clock2, clock2])
+    await asyncio.gather(*row1, *row2, *col1, *col2, *row3)  
 
 curses.wrapper(lambda stdscr: asyncio.run(main(stdscr)))
-
-
 
