1a19ae14c60ac70248789943270df96a4eb79e63
sumayya55 <sumayya.kamal55@gmail.com>
2020-10-31 20:36:48 +0300

    changes to get a better level of abstraction

--- i18ned_clock/app.py
+++ i18ned_clock/app.py
@@ -2,12 +2,11 @@ import curses
 import asyncio
 from datetime import datetime
 import pytz
-import os
-  
 
-class Clock(object):
+
+class Clock:
     def __init__(self, timezone):
-        self.tz = pytz.timezone(timezone)
+        self.tz = pytz.timezone(timezone)      
 
     def paint(self, frame):
         if frame >= 30:
@@ -17,7 +16,7 @@ class Clock(object):
 
 
 async def init_screen(stdscr, clock, y=0, x=0):
-    curses.curs_set(0) 
+    curses.curs_set(0)
 
     frame = 0
     num = 0
@@ -32,46 +31,64 @@ async def init_screen(stdscr, clock, y=0, x=0):
                 break
         except:
             raise Exception('Window size is small.')
-            
 
-class Tiles(object):
+
+class Screen:
     y = 0
     x = 0
-    
-    def __init__(self, stdscr):
-        self.stdscr = stdscr
-   
-    async def row(self, clocklist):
-        resultedCoroutines = []
-        x = Tiles.x
-        for c in clocklist:
-            resultedCoroutines.append(init_screen(self.stdscr, c, y=Tiles.y, x=x))
-            x += 30
-        Tiles.y += 3
-        return resultedCoroutines
+    async def init_screen(stdscr, clock, y=y, x=x):
+        curses.curs_set(0) 
 
-    async def column(self, clocklist):
-        resultedCoroutines = []
-        y = Tiles.y
-        for c in clocklist:
-            resultedCoroutines.append(init_screen(self.stdscr, c, y=y, x=Tiles.x))
-            y += 3
-        Tiles.x += 30
-        return resultedCoroutines
+        frame = 0
+        num = 0
+        while True:
+            try:
+                stdscr.addstr(y, x, clock.paint(frame), curses.A_BOLD)
+                await asyncio.sleep(1/60)
+                stdscr.refresh()
+                frame = (frame + 1) % 60
+                num += 1
+                if num == 240:
+                    break
+            except:
+                raise Exception('Window size is small.')
+
+
+resultedCoroutines = []
+
+
+class Row(Screen):
+    def __init__(self, stdscr, clocks):
+        self.clocks = clocks
+        self.stdscr = stdscr
+
+    async def paint(self):
+        for clock in self.clocks:
+            resultedCoroutines.append(Screen.init_screen(self.stdscr, clock, Screen.y, Screen.x))
+            Screen.x += 30
+        Screen.x = 0
+        Screen.y += 4   
+
+
+class Column(Screen):
+    def __init__(self, stdscr, clocks):
+        self.clocks = clocks
+        self.stdscr = stdscr
+
+    async def paint(self):
+        for clock in self.clocks:
+            resultedCoroutines.append(Screen.init_screen(self.stdscr, clock, Screen.y, Screen.x))
+            Screen.y += 3
+        Screen.x = 0
+        Screen.y += 1
 
 
 clock1 = Clock("Asia/Baghdad")
 clock2 = Clock("Asia/Kabul")
 
-async def main(stdscr):
-    t = Tiles(stdscr) 
 
-    row1 = await t.row([clock1, clock2, clock1, clock2, clock1])
-    row2 = await t.row([clock1, clock1, clock1, clock1, clock1])
-    col1 = await t.column([clock1, clock2, clock1])
-    col2 = await t.column([clock1, clock2, clock1])
-    row3 = await t.row([clock1, clock1, clock1, clock2, clock2])
-    await asyncio.gather(*row1, *row2, *col1, *col2, *row3)  
+async def main(stdscr):
+    Column(stdscr, clocks=[Row(stdscr, clocks=[await Column(stdscr, clocks=[clock1, clock2, clock1, clock1]).paint()]), await Row(stdscr, clocks=[clock1, clock2, clock1]).paint(), await Column(stdscr, clocks=[clock1, clock2, clock1]).paint(), await Row(stdscr, clocks=[clock1, clock2, clock1, clock1]).paint()])
+    await asyncio.gather(*resultedCoroutines) 
 
 curses.wrapper(lambda stdscr: asyncio.run(main(stdscr)))
-
