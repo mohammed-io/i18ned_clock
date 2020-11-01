5bee239d15017172cf910cf8d76a6104946e5a17
Mohammed Abdulkereem <mohammed-io@outlook.com>
2020-11-01 11:02:02 +0300

    Improved the example

--- i18ned_clock/app.py
+++ i18ned_clock/app.py
@@ -32,25 +32,6 @@ async def init_screen(stdscr, clock, y=0, x=0):
         except:
             raise Exception('Window size is small.')
 
-
-    async def init_screen(stdscr, clock, y=y, x=x):
-        curses.curs_set(0) 
-
-        frame = 0
-        num = 0
-        while True:
-            try:
-                stdscr.addstr(y, x, clock.paint(frame), curses.A_BOLD)
-                await asyncio.sleep(1/60)
-                stdscr.refresh()
-                frame = (frame + 1) % 60
-                num += 1
-                if num == 240:
-                    break
-            except:
-                raise Exception('Window size is small.')
-
-
 class Column:
     def __init__(self, children):
         self.children = children
@@ -72,6 +53,14 @@ clock2 = Clock("Asia/Kabul")
 
 
 async def main(stdscr):
-    await asyncio.gather(init_screen(stdscr, Column(children=[clock1, clock2, clock1]), 0, 0), init_screen(stdscr, Row(children=[clock1, clock1]), 5, 0),  init_screen(stdscr, Row(children=[clock1, clock1, clock1, clock1]), 8, 20))
+    await asyncio.gather(
+            init_screen(stdscr,
+             Column(children=[
+                 Row(children=[clock1, clock2, clock1]),
+                 Clock("Asia/Baghdad"),
+                 Row(children=[clock1, clock1]),
+                 Row(children=[clock1, clock1, clock1, clock1]),
+            ]))
+        )
 
 curses.wrapper(lambda stdscr: asyncio.run(main(stdscr)))
